6f52eb597b1f01dba1618ce502a8122c52b35320
Burhanuddin-Innovative <burhanuddin.halai@innovative-net.com>
2025-05-05 19:41:55 +0500

    Refactor second_weighbridges migration to enhance schema design
    - Updated the second_weighbridges table schema to include nullable fields for company_id, arrival_ticket_id, weight, remark, and created_by.
    - Added soft deletes and timestamps for better data management.
    - Removed obsolete migration file for altering the second_weighbridges table.

--- database/migrations/2025_03_24_075818_create_second_weighbridges_table.php
+++ database/migrations/2025_03_24_075818_create_second_weighbridges_table.php
@@ -12,13 +12,17 @@ return new class extends Migration {
     {
         Schema::create('second_weighbridges', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('company_id');
-            $table->string('weight');
-            $table->longText('remarks');
+            $table->unsignedBigInteger('company_id')->nullable();
+            $table->unsignedBigInteger('arrival_ticket_id')->nullable();
+            $table->string('weight')->nullable();
+            $table->string('remark')->nullable();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
 
             $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
-
-            $table->timestamps();
+            $table->foreign('arrival_ticket_id')->references('id')->on('arrival_tickets')->onDelete('cascade');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
