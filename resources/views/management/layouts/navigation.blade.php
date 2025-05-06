9e3d94dd34248be8f97af50e31277175b40a68ec
naveed-ul-llah <naveed@Innovative.net>
2025-05-06 17:29:46 +0500

    fff

--- resources/views/management/layouts/navigation.blade.php
+++ resources/views/management/layouts/navigation.blade.php
@@ -170,7 +170,7 @@
                             </span></a>
                     </li>
                     <li data-menu=""><a class="dropdown-item d-flex align-items-center"
-                            href="{{ route('sampling-monitoring.index') }}" data-toggle="dropdown"><i
+                            href="{{ route('raw-material.purchase-order.index') }}" data-toggle="dropdown"><i
                                 class="ft-arrow-right submenu-icon"></i><span data-i18n="Email">Purchase Order
                             </span></a>
                     </li>
