82ee49fee81f9f2940eb559e5aeb11faef3b681b
Burhanuddin-Innovative <burhanuddin.halai@innovative-net.com>
2025-05-15 11:40:37 +0500

    Enhance arrival ticket migration and create millers table
    - Added new fields to the arrival_tickets table, including broker_id, accounts_of_name, and bilty_return fields.
    - Established foreign key relationships for broker_id and miller_id.
    - Created a new migration for the millers table with soft deletes and timestamps.
    - Cleaned up the arrival_tickets migration by removing unnecessary comments and code.

--- resources/views/management/arrival/arrival_slip/getList.blade.php
+++ resources/views/management/arrival/arrival_slip/getList.blade.php
@@ -31,7 +31,6 @@
                         <p class="m-0">
                             {{ \Carbon\Carbon::parse($row->created_at)->format('Y-m-d') }} /
                             {{ \Carbon\Carbon::parse($row->created_at)->format('H:i A') }} <br>
-
                         </p>
                     </td>
                     <td>
