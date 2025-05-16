a005e8b6f93608d020942c9e28820718f50000ff
Burhanuddin-Innovative <burhanuddin.halai@innovative-net.com>
2025-05-16 16:46:21 +0500

    asd

--- resources/views/management/procurement/raw_material/purchase_order/edit.blade.php
+++ resources/views/management/procurement/raw_material/purchase_order/edit.blade.php
@@ -343,6 +343,7 @@
              </div>
          </div>
          <div class="col-xs-6 col-sm-6 col-md-6">
+
              <div class="form-group">
                  <label>Delivery Address:</label>
                  <input type="text" name="delivery_address" value="{{ $arrivalPurchaseOrder->delivery_address }}"
