64337640d34441502e5ba35c0c4cc6198ade8a33
Burhanuddin-Innovative <burhanuddin.halai@innovative-net.com>
2025-06-02 12:32:46 +0500

    asd

--- resources/views/management/arrival/freight/partials/freight_form.blade.php
+++ resources/views/management/arrival/freight/partials/freight_form.blade.php
@@ -219,7 +219,7 @@
     $('.calculate-net-shortage').on('input', function() {
         const exemptedWeight = parseFloat($(this).val()) || 0;
         const netShortage = parseFloat(
-            "{{ ($ticket->net_weight ?? 0) - ($ticket->arrived_net_weight ?? 0) }}") - exemptedWeight;
+            "{{ ($ticket->arrived_net_weight ?? 0) - ($ticket->net_weight ?? 0) }}") - exemptedWeight;
         $('input[name="net_shortage"]').val(netShortage);
         calculateFinalAmounts();
     });
