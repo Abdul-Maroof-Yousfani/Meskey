801dd88940d1cb50e26aaa79caaf08b155e561e7
Burhanuddin-Innovative <burhanuddin.halai@innovative-net.com>
2025-06-11 16:02:21 +0500

    asd

--- app/Http/Controllers/Arrival/SamplingMonitoringController.php
+++ app/Http/Controllers/Arrival/SamplingMonitoringController.php
@@ -30,6 +30,7 @@ class SamplingMonitoringController extends Controller
             ->where('is_done', 'yes')
             ->when($request->filled('search'), function ($q) use ($request) {
                 $searchTerm = '%' . $request->search . '%';
+
                 return $q->where(function ($sq) use ($searchTerm) {
                     $sq->orWhereHas('arrivalTicket', function ($aq) use ($searchTerm) {
                         $aq->where('unique_no', 'like', $searchTerm)
@@ -42,17 +43,15 @@ class SamplingMonitoringController extends Controller
             })
             ->where(function ($q) {
                 $q->where('approved_status', 'pending')
-                    ->orWhere(function ($q) {
-                        $q->where('decision_making', 1)
-                            ->where('lumpsum_deduction', 0)
-                            ->where('lumpsum_deduction_kgs', 0);
-                    });
+                    ->orWhere('decision_making', 1);
             })
             ->latest()
+            // ->orderBy('created_at', 'asc')
             ->paginate(request('per_page', 25));
 
         return view('management.arrival.sampling_monitoring.getList', compact('samplingRequests'));
     }
+
 
     /**
      * Show the form for creating a new resource.
