87fea2d3c5fc18cd895da4c0eb35b7e94538cbdb
naveed-ul-llah <naveed@Innovative.net>
2025-07-25 18:29:36 +0500

    Fixes Pohouch Calc

--- resources/views/management/procurement/raw_material/ticket_payment_request/index.blade.php
+++ resources/views/management/procurement/raw_material/ticket_payment_request/index.blade.php
@@ -84,7 +84,7 @@
                                             <th class="col-sm-2">Commodity</th>
                                             <th class="col-sm-1">Loading date</th>
                                             <th class="col-sm-2">Amounts</th>
-                                            <th class="col-sm-1">Total Req Amount</th>
+                                            <th class="col-sm-1">Tot. Req. Amt.</th>
                                             <th class="col-sm-1">Created</th>
                                             <th class="col-sm-1">Action</th>
                                         </tr>
