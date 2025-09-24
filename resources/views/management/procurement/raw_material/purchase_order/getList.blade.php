3d9aad58839c6def8e2deb470afa9535c0ce1225
naveed-ul-llah <naveed@Innovative.net>
2025-09-25 03:02:40 +0500

    Purchase Order Replacement/No Replacement Calc fixed

--- resources/views/management/procurement/raw_material/purchase_order/getList.blade.php
+++ resources/views/management/procurement/raw_material/purchase_order/getList.blade.php
@@ -1,105 +1,116 @@
 <x-sticky-table :items="$arrivalPurchaseOrder" :leftSticky="3" :rightSticky="1" :emptyMessage="'No purchase orders found'" :pagination="$arrivalPurchaseOrder->links()">
     @slot('head')
-        <th>Contract #</th>
-        <th>Commodity</th>
-        <th>Supplier Name</th>
-        <th>Broker</th>
-        <th>Decision Of</th>
-        <th>Rate</th>
-        <th>Expiry Date</th>
-        <th>Sauda Type</th>
-        <th>Replacement</th>
-        <th>Remarks</th>
-        <th>No of Trucks</th>
-        <th>Ordered QTY</th>
-        <th>Arrived Trucks</th>
-        <th>Arrived QTY</th>
-        <th>Balance Trucks</th>
-        <th>Balance Quantity</th>
-        <th>Stock in Transit Trucks</th>
-        <th>Rejected Trucks</th>
-        <th>Status</th>
-        <th>Action</th>
+    <th>Contract #</th>
+    <th>Commodity</th>
+    <th>Supplier Name</th>
+    <th>Broker</th>
+    <th>Decision Of</th>
+    <th>Rate</th>
+    <th>Expiry Date</th>
+    <th>Sauda Type</th>
+    <th>Replacement</th>
+    <th>Remarks</th>
+    <th>No of Trucks</th>
+    <th>Ordered QTY</th>
+    <th>Arrived Trucks</th>
+    <th>Arrived QTY</th>
+    <th>Balance Trucks</th>
+    <th>Balance Quantity</th>
+    <th>Stock in Transit Trucks</th>
+    <th>Rejected Trucks</th>
+    <th>Status</th>
+    <th>Action</th>
     @endslot
 
     @slot('body')
-        @foreach ($arrivalPurchaseOrder as $row)
-            @php
-              //  $arrivedTrucks = $row->arrivalTickets()->sum('closing_trucks_qty');
-                $arrivedTrucks = $row->approvedArrivalTickets()->sum('closing_trucks_qty');
-                $rejectedTrucks = $row->rejectedArrivalTickets->count();
-                $inTransitTrucks = $row->stockInTransitTickets->count();
-                $orderedTrucks = $row->no_of_trucks ?? 0;
+    @foreach ($arrivalPurchaseOrder as $row)
+        @php
+            //  $arrivedTrucks = $row->arrivalTickets()->sum('closing_trucks_qty');
+            $arrivedTrucks = $row->approvedArrivalTickets()->sum('closing_trucks_qty');
+            $rejectedTrucks = $row->rejectedArrivalTickets->count();
+            $inTransitTrucks = $row->stockInTransitTickets->count();
+            $orderedTrucks = $row->no_of_trucks ?? 0;
 
-                if ($row->is_replacement == 1) {
-                    $balanceTrucks = $orderedTrucks - $arrivedTrucks - $inTransitTrucks;
-                } else {
-                    $balanceTrucks = $orderedTrucks - $arrivedTrucks - $inTransitTrucks - $rejectedTrucks;
-                }
-            @endphp
+            if ($row->is_replacement == 1) {
+                $balanceTrucks = $orderedTrucks - $arrivedTrucks - $inTransitTrucks;
+            } else {
+                $balanceTrucks = $orderedTrucks - $arrivedTrucks - $inTransitTrucks - $rejectedTrucks;
+            }
+        @endphp
 
-            <tr>
-                <td>
-                    #{{ $row->contract_no }}
-                </td>
-                <td>{{ $row->product->name ?? 'N/A' }}</td>
-                <td>{{ $row->purchase_type == 'gate_buying' ? $row->supplier_name ?? 'N/A' : $row->supplier->name ?? 'N/A' }}
-                </td>
-                <td>{{ $row->broker_one_name ?? ($row->broker_two_name ?? ($row->broker_three_name ?? 'N/A')) }}</td>
-                <td>{{ $row->createdByUser->name ?? 'N/A' }}</td>
-                <td>
-                    {{ $row->rate_per_kg ?? 'N/A' }}
-                    <div class="d-none div-box-b">
-                        <small>
-                            <strong>KG:</strong> {{ $row->rate_per_kg ?? 0 }}<br>
-                            <strong>Mound:</strong> {{ $row->rate_per_mound ?? 0 }}<br>
-                            <strong>100KG:</strong> {{ $row->rate_per_100kg ?? 0 }}
-                        </small>
-                    </div>
-                </td>
-                <td>{{ $row->delivery_date ? \Carbon\Carbon::parse($row->delivery_date)->format('d-m-Y') : 'N/A' }}</td>
-                <td>
-                    @if (isset($row->saudaType->name) && $row->saudaType->name == 'Thadda')
-                        <span class="badge badge-primary">{{ $row->saudaType->name }}</span>
-                    @else
-                        <span class="badge badge-secondary">{{ $row->saudaType->name ?? '' }}</span>
-                    @endif
-                </td>
-                <td>
-                    <span class="badge badge-{{ $row->is_replacement == 1 ? 'success' : 'warning' }}">
-                        {{ $row->is_replacement == 1 ? 'Yes' : 'No' }}
-                    </span>
-                </td>
-                <td>{{ $row->remarks ?? 'N/A' }}</td>
-                <td>{{ $row->no_of_trucks ?? 0 }}</td>
-                <td>{{ (isset($row->min_quantity) ? intval($row->min_quantity) : '-') . ' - ' . (isset($row->max_quantity) ? intval($row->max_quantity) : '-') }}
-                </td>
-                <td>{{ $arrivedTrucks }}</td>
-                <td>{{ $row->totalArrivedNetWeight->total_arrived_net_weight ?? 0 }}</td>
-                <td>{{ $balanceTrucks }}</td>
-                <td>
-                    {{ (($row->min_quantity ?? 0) - ($row->totalArrivedNetWeight->total_arrived_net_weight ?? 0) ?? '-') . ' - ' . (($row->max_quantity ?? 0) - ($row->totalArrivedNetWeight->total_arrived_net_weight ?? 0) ?? '-') }}
-                </td>
-                <td>{{ $row->stockInTransitTickets->count() }}</td>
-                <td>{{ $rejectedTrucks }}</td>
-                <td>
-                    @if ($row->status == 'completed')
-                        <span class="badge badge-success">Closed</span>
-                    @else
-                        <span class="badge badge-warning">Pending</span>
-                    @endif
-                </td>
-                <td>
-                    <a onclick="openModal(this,'{{ route($row->purchase_type == 'gate_buying' ? 'raw-material.gate-buying.edit' : 'raw-material.purchase-order.edit', $row->id) }}','{{ $row->purchase_type == 'gate_buying' ? 'Edit Gate Buying' : 'Edit Purchase Order' }}')"
-                        class="info p-1 text-center mr-2 position-relative">
-                        <i class="ft-edit font-medium-3"></i>
-                    </a>
-                    <a onclick="deletemodal('{{ route('raw-material.purchase-order.destroy', $row->id) }}', '{{ route('raw-material.get.purchase-order') }}')"
-                        class="danger p-1 text-center mr-2 position-relative">
-                        <i class="ft-x font-medium-3"></i>
-                    </a>
-                </td>
-            </tr>
-        @endforeach
+        <tr>
+            <td>
+                #{{ $row->contract_no }}
+            </td>
+            <td>{{ $row->product->name ?? 'N/A' }}</td>
+            <td>{{ $row->purchase_type == 'gate_buying' ? $row->supplier_name ?? 'N/A' : $row->supplier->name ?? 'N/A' }}
+            </td>
+            <td>{{ $row->broker_one_name ?? ($row->broker_two_name ?? ($row->broker_three_name ?? 'N/A')) }}</td>
+            <td>{{ $row->createdByUser->name ?? 'N/A' }}</td>
+            <td>
+                {{ $row->rate_per_kg ?? 'N/A' }}
+                <div class="d-none div-box-b">
+                    <small>
+                        <strong>KG:</strong> {{ $row->rate_per_kg ?? 0 }}<br>
+                        <strong>Mound:</strong> {{ $row->rate_per_mound ?? 0 }}<br>
+                        <strong>100KG:</strong> {{ $row->rate_per_100kg ?? 0 }}
+                    </small>
+                </div>
+            </td>
+            <td>{{ $row->delivery_date ? \Carbon\Carbon::parse($row->delivery_date)->format('d-m-Y') : 'N/A' }}</td>
+            <td>
+                @if (isset($row->saudaType->name) && $row->saudaType->name == 'Thadda')
+                    <span class="badge badge-primary">{{ $row->saudaType->name }}</span>
+                @else
+                    <span class="badge badge-secondary">{{ $row->saudaType->name ?? '' }}</span>
+                @endif
+            </td>
+            <td>
+                <span class="badge badge-{{ $row->is_replacement == 1 ? 'success' : 'warning' }}">
+                    {{ $row->is_replacement == 1 ? 'Yes' : 'No' }}
+                </span>
+            </td>
+            <td>{{ $row->remarks ?? 'N/A' }}</td>
+            <td>
+                @if($row->calculation_type == 'trucks')
+                    {{ $row->no_of_trucks ?? 0 }}
+                @else
+                    N/A
+                @endif
+            </td>
+            <td>{{ (isset($row->min_quantity) ? intval($row->min_quantity) : '-') . ' - ' . (isset($row->max_quantity) ? intval($row->max_quantity) : '-') }}
+            </td>
+            <td>{{ $arrivedTrucks }}</td>
+            <td>{{ $row->totalArrivedNetWeight->total_arrived_net_weight ?? 0 }}</td>
+            <td>
+                @if($row->calculation_type == 'trucks')
+                    {{ $balanceTrucks }}
+                @else
+                    N/A
+                @endif
+            <td>
+                {{ (($row->min_quantity ?? 0) - ($row->totalArrivedNetWeight->total_arrived_net_weight ?? 0) ?? '-') . ' - ' . (($row->max_quantity ?? 0) - ($row->totalArrivedNetWeight->total_arrived_net_weight ?? 0) ?? '-') }}
+            </td>
+            <td>{{ $row->stockInTransitTickets->count() }}</td>
+            <td>{{ $rejectedTrucks }}</td>
+            <td>
+                @if ($row->status == 'completed')
+                    <span class="badge badge-success">Closed</span>
+                @else
+                    <span class="badge badge-warning">Pending</span>
+                @endif
+            </td>
+            <td>
+                <a onclick="openModal(this,'{{ route($row->purchase_type == 'gate_buying' ? 'raw-material.gate-buying.edit' : 'raw-material.purchase-order.edit', $row->id) }}','{{ $row->purchase_type == 'gate_buying' ? 'Edit Gate Buying' : 'Edit Purchase Order' }}')"
+                    class="info p-1 text-center mr-2 position-relative">
+                    <i class="ft-edit font-medium-3"></i>
+                </a>
+                <a onclick="deletemodal('{{ route('raw-material.purchase-order.destroy', $row->id) }}', '{{ route('raw-material.get.purchase-order') }}')"
+                    class="danger p-1 text-center mr-2 position-relative">
+                    <i class="ft-x font-medium-3"></i>
+                </a>
+            </td>
+        </tr>
+    @endforeach
     @endslot
 </x-sticky-table>
