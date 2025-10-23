9618b3d8d6fb04fae58471da12280fc6df67a43c
Naveed Ullah <naveed@innovative.net>
2025-10-23 10:17:30 +0500

    Update Freight Party label in freight request form to display payment_to value

--- resources/views/management/procurement/raw_material/freight_request/create.blade.php
+++ resources/views/management/procurement/raw_material/freight_request/create.blade.php
@@ -116,7 +116,7 @@
         </div>
         <div class="col-md-4">
             <div class="form-group">
-                <label class="font-weight-bold">Freight Party</label>
+                <label class="font-weight-bold">Freight Party {{ $freightPaymentRequest->payment_to }}</label>
                 <select class="form-control editable-field select2" name="vendor_id" @disabled($param0)>
                     <option value="">Select Freight Party</option>
                     @foreach ($vendors as $vendor)
