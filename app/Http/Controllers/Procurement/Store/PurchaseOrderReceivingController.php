debc9543ca1374fe9f91e9ef0e31112287149d7a
HashimAbbas555 <hashim.abbas@innovative-net.com>
2025-11-17 11:54:28 +0500

    test

--- app/Http/Controllers/Procurement/Store/PurchaseOrderReceivingController.php
+++ app/Http/Controllers/Procurement/Store/PurchaseOrderReceivingController.php
@@ -222,7 +222,7 @@ class PurchaseOrderReceivingController extends Controller
 
 
         $categories = Category::select('id', 'name')->where('category_type', 'general_items')->get();
-        $job_orders = JobOrder::select('id', 'name')->get();
+        $job_orders = JobOrder::select('id', 'job_order_name')->get();
        
         $html = view('management.procurement.store.purchase_order_receiving.purchase_data', compact('dataItems', 'categories', 'job_orders'))->render();
 
