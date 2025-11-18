222d487509ab64e7221d92bc85bc5a217f270eed
HashimAbbas555 <hashim.abbas@innovative-net.com>
2025-11-18 13:02:43 +0500

    Delete al qcs

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Master\ArrivalLocationController;
 use App\Http\Controllers\Master\ProductSlabController;
 use App\Models\Procurement\Store\PurchaseOrder;
 use App\Models\Procurement\Store\PurchaseOrderData;
+use App\Models\Procurement\Store\PurchaseOrderReceiving;
+use App\Models\Procurement\Store\PurchaseOrderReceivingData;
 use App\Models\Procurement\Store\PurchaseQuotation;
 use App\Models\Procurement\Store\PurchaseQuotationData;
 use App\Models\Procurement\Store\PurchaseRequest;
@@ -31,10 +33,14 @@ Route::get("/restore-db", function() {
     $purchase_request = PurchaseRequest::query()->delete();
     $purchase_request_data = PurchaseRequestData::query()->delete();
     $purchase_quotation = PurchaseQuotation::query()->delete();
+    $purchase_receive = PurchaseOrderReceiving::query()->delete();
 
     $purchase_quotation_data = PurchaseQuotationData::query()->delete();
     $purchase_order = PurchaseOrder::query()->delete();
     $purchase_order_data = PurchaseOrderData::query()->delete();
+    $purchase_receive_data = PurchaseOrderReceivingData::query()->delete();
+    $qc = QC::query()->delete();
+    $qc_bags = QCBags::query()->delete();
 
     DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
