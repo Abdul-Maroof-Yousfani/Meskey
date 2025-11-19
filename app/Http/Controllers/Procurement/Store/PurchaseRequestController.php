4e171a1815bd811ad0c73bb325327d8c3b2eb1ef
HashimAbbas555 <hashim.abbas@innovative-net.com>
2025-11-19 15:19:48 +0500

    Revert

--- app/Http/Controllers/Procurement/Store/PurchaseRequestController.php
+++ app/Http/Controllers/Procurement/Store/PurchaseRequestController.php
@@ -155,7 +155,6 @@ class PurchaseRequestController extends Controller
                 ]);
                 if (!empty($request->job_order_id[$index]) && is_array($request->job_order_id[$index])) {
                     foreach ($request->job_order_id[$index] as $jobOrderId) {
-                        $arr[$index] = [$purchaseRequest->id, $requestData->id];
                         PurchaseAgainstJobOrder::create([
                             'purchase_request_id' => $purchaseRequest->id,
                             'purchase_request_data_id' => $requestData->id,
@@ -164,7 +163,6 @@ class PurchaseRequestController extends Controller
                     }
                 }
             }
-            dd($arr);
             
             DB::commit();
 
