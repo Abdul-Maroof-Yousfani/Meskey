b001bbd8009958043fe68bec562ab0bf05b636f7
HashimAbbas555 <hashim.abbas@innovative-net.com>
2025-11-19 16:45:21 +0500

    test

--- app/Http/Controllers/Procurement/Store/PurchaseQuotationController.php
+++ app/Http/Controllers/Procurement/Store/PurchaseQuotationController.php
@@ -332,7 +332,7 @@ class PurchaseQuotationController extends Controller
                     $itemRowspan = count($itemGroup['suppliers']);
                     $quotaionCount = count($requestGroup['quotations']);
                     $requestRowspan += $itemRowspan;
-                    $quotaionRowspan += ($quotaionCount);
+                    $quotaionRowspan += ($quotaionCount + $requestRowspan);
 
                     $itemSuppliers = [];
                     $isFirstSupplier = true;
