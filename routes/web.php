f369378c81dc6cd531ea4c65751f7ee02245ecd3
HashimAbbas555 <hashim.abbas@innovative-net.com>
2025-12-01 12:36:55 +0500

    Add data in customers table

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Arrival\ArrivalSlipController;
 use App\Http\Controllers\Master\ArrivalLocationController;
 use App\Http\Controllers\Master\ProductSlabController;
+use App\Models\Master\Customer;
 use App\Models\Procurement\Store\PurchaseBill;
 use App\Models\Procurement\Store\PurchaseBillData;
 use App\Models\Procurement\Store\PurchaseOrder;
@@ -31,8 +32,12 @@ use App\Http\Controllers\Reports\{
 use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Permission;
 
-Route::get("checking-data", function() {
-    dd(QC::all());
+Route::get("add-customer", function() {
+    Customer::create([
+        "name" => "Hashim",
+        "company_id" => 1,
+        "status" => "active"
+    ]);
 });
 
 
