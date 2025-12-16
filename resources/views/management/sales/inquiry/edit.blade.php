078765c78f37c8d7c55d26ae7d722bb2ec9aaf61
HashimAbbas555 <hashim.abbas@innovative-net.com>
2025-12-16 12:46:32 +0500

    testing

--- resources/views/management/sales/inquiry/edit.blade.php
+++ resources/views/management/sales/inquiry/edit.blade.php
@@ -307,7 +307,7 @@
         }
 
         // No of bags = bag size * quantity (per requirement)
-        const result = bag_size / qty;
+        const result = (bag_size / qty).toFixed();
         no_of_bags.val(result);
     }
 
