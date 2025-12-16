18dc520ea9f9247a01904782009a4a75147ec34b
Arsal-INPL <arsal.farooq@innovative-net.com>
2025-12-16 16:54:01 +0500

    numeric_code remove from countries

---
+++ database/migrations/2025_12_16_165319_remove_numeric_code_from_countries_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('countries', function (Blueprint $table) {
+            if (Schema::hasColumn('countries', 'numeric_code')) {
+                $table->dropColumn('numeric_code');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('countries', function (Blueprint $table) {
+            $table->string('numeric_code', 3)->nullable();
+        });
+    }
+};
