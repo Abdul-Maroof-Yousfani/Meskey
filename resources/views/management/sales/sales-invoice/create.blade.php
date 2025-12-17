254652cb5c11fe4415a31c1d736d0a7e9e7423de
HashimAbbas555 <hashim.abbas@innovative-net.com>
2025-12-17 11:09:57 +0500

    Sale invoice

--- resources/views/management/sales/sales-invoice/create.blade.php
+++ resources/views/management/sales/sales-invoice/create.blade.php
@@ -380,7 +380,7 @@
         const gstPercent = parseFloat(gstPercentInput.val()) || 0;
 
         // Calculate Qty = Packing * No of Bags
-        const result = (parseFloat(packingInput.val()) / parseFloat(qtyInput.val())).toFixed();
+        const result = (parseFloat(qtyInput.val()) / parseFloat(packingInput.val())).toFixed();
         noOfBagsInput.val(result);
       
         // Calculate Gross Amount = Qty * Rate
