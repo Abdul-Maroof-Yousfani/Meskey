3f611638b23ab945b85774392e6ea036309e249d
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-01-07 17:22:49 +0500

    Update getList.blade.php

--- resources/views/management/master/product/getList.blade.php
+++ resources/views/management/master/product/getList.blade.php
@@ -5,6 +5,7 @@
             <th class="col-sm-3">Name </th>
             <th class="col-sm-3">Parent</th>
             <th class="col-sm-1">Product Type</th>
+            <th class="col-sm-1">Category</th>
             <th class="col-sm-1">Status</th>
             <th class="col-sm-2">Created</th>
             <th class="col-sm-1">Action</th>
@@ -20,6 +21,7 @@
                     <td style="background: #f7f7f7; font-weight: bold;">{{ $parent->name }}</td>
                     <td>—</td>
                     <td><small>{{ formatEnumValue($parent->product_type) ?? '--' }}</small></td>
+                    <td><small>{{ get_category_name($parent->category_id) }}</small></td>
                     <td><label
                             class="badge bg-light-{{ $parent->status == 'inactive' ? 'danger' : 'success' }}">{{ $parent->status }}</label>
                     </td>
