e3de0ae6e26dad380006e72ac95e5d87a3edd2a9
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-01-12 11:55:26 +0500

    test

--- resources/views/management/arrival/approved_arrival/create.blade.php
+++ resources/views/management/arrival/approved_arrival/create.blade.php
@@ -16,7 +16,7 @@
                             Ticket No: {{ $arrivalTicket->unique_no }} --
                             Truck No: {{ $arrivalTicket->truck_no ?? '-' }} --
                             Arrival Location: {{ $arrivalTicket->unloadingLocation->arrivalLocation?->name ?? 'N' }}
-                            ID: {{ $arrivalTicket->arrival_location_id }}
+                            ID: {{ $arrivalTicket->unloadingLocation->arrivalLocation?->id ?? 'N' }}
                         </option>
                     @endforeach
                 </select>
