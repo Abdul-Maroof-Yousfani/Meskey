995538f39e40acf68c56fbcf2520b9dd3f882eba
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-01-12 12:10:20 +0500

    test

--- app/Http/Requests/Master/ArrivalSubLocationRequest.php
+++ app/Http/Requests/Master/ArrivalSubLocationRequest.php
@@ -22,7 +22,7 @@ class ArrivalSubLocationRequest extends FormRequest
      */
     public function rules(): array
     {
-        dd($this->arrival_location_id);
+        dd($this->id);
         return [
             'company_id' => 'required|exists:companies,id',
             'company_location_id' => 'required|exists:company_locations,id',
@@ -34,7 +34,7 @@ class ArrivalSubLocationRequest extends FormRequest
 
                 Rule::unique('arrival_sub_locations', 'name')
                     ->where('company_id', $this->input('company_id'))
-                    ->ignore($this->arrival_location_id)
+                    ->ignore($this->id)
             ],
             'description' => 'nullable|string|max:500',
             'status' => ['required', Rule::in(['active', 'inactive'])],
