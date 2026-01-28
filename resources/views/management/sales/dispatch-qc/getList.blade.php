e9f11589922b63b1f8c2011c178c8dd6fff73d1b
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-01-28 13:18:56 +0500

    Update getList.blade.php
    removed edit, and delete functionality

--- resources/views/management/sales/dispatch-qc/getList.blade.php
+++ resources/views/management/sales/dispatch-qc/getList.blade.php
@@ -35,24 +35,12 @@
                         {{ $dispatchQc->created_at->format('d-m-Y H:i') }}
                     </td>
                     <td>
-                        <a onclick="openModal(this,'{{ route('sales.dispatch-qc.edit', $dispatchQc->id) }}','Edit Dispatch QC', false)"
-                            class="warning p-1 text-center mr-2 position-relative" title="Edit">
-                            <i class="ft-edit font-medium-3"></i>
-                        </a>
+                       
                         <a onclick="openModal(this,'{{ route('sales.dispatch-qc.show', $dispatchQc->id) }}','View Dispatch QC', true)"
                             class="info p-1 text-center mr-2 position-relative" title="View">
                             <i class="ft-eye font-medium-3"></i>
                         </a>
-                        {{-- @if($dispatchQc->status == 'accept')
-                            <a onclick="openModal(this,'{{ route('sales.get.dispatch-qc.gate-out', $dispatchQc->id) }}', 'Gate Out', true, '100%')"
-                                class="success p-1 text-center mr-2 position-relative" title="Gate Out Pass" style="cursor: pointer;">
-                                <i class="ft-file font-medium-3"></i>
-                            </a>
-                        @endif --}}
-                        <a onclick="deletemodal('{{ route('sales.dispatch-qc.destroy', $dispatchQc->id) }}', '{{ route('sales.get.dispatch-qc') }}')"
-                            class="danger p-1 text-center mr-2 position-relative" title="Delete">
-                             <i class="ft-trash-2 font-medium-3"></i>
-                         </a>
+                      
                          
                     </td>
                 </tr>
