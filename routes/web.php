d35ccfbea8c1d0a59ce81f46763a017479f9d56f
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-02-04 11:08:12 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ use App\Models\Production\JobOrder\JobOrder;
 use App\Models\ReceiptVoucher;
 use App\Models\Sales\DeliveryChallan;
 
+use App\Models\Supplier;
+
 use App\Models\Sales\DeliveryOrder;
 use App\Models\Sales\FirstWeighbridge;
 use App\Models\Sales\LoadingProgram;
