8daa1c5d4d2ae4888464a38f2c5f6cfc409cd642
naveed-ul-llah <naveed@Innovative.net>
2026-02-04 14:19:04 +0500

    Dahsboard Api Fixed

--- app/Http/Controllers/API/Arrival/InnerSampleRequestController.php
+++ app/Http/Controllers/API/Arrival/InnerSampleRequestController.php
@@ -26,11 +26,12 @@ class InnerSampleRequestController extends Controller
                         ->where('document_approval_status', '!=', 'half_approved')
                         ->orWhereNull('document_approval_status');
                 })
+                ->where('second_qc_status', '!=', 'rejected')
                 ->leftJoin('arrival_sampling_requests', function ($join) {
                     $join->on('arrival_tickets.id', '=', 'arrival_sampling_requests.arrival_ticket_id')
-                        ->where('sampling_type', 'inner')
-                        ->where('approved_status', 'pending')
-                        ->where('arrival_sampling_requests.deleted_at', null);
+                    ->where('sampling_type', 'inner')
+                    ->where('approved_status', 'pending')
+                    ->where('arrival_sampling_requests.deleted_at', null);
                 })
                 // ->when(!$isSuperAdmin, function ($q) use ($authUser) {
                 //     return $q->whereHas('unloadingLocation', function ($query) use ($authUser) {
