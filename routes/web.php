0800e6c13c8d0444afafa8455c5b834baba805e4
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-02-04 11:26:22 +0500

    voucher types create

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Master\ArrivalLocationController;
 use App\Http\Controllers\Master\ProductSlabController;
 use App\Models\Category;
 use App\Models\Master\Account\Account;
+use App\Models\Master\Account\Transaction;
 use App\Models\Master\Account\TransactionVoucherType;
 use App\Models\Master\Customer;
 use App\Models\Procurement\Store\PurchaseBill;
@@ -59,8 +60,17 @@ Route::get("/receipt-vouchers/delete", function() {
 });
 
 Route::get("voucher-types", function() {
-    $transactions = TransactionVoucherType::all();
-    dd($transactions);
+    Transaction::create([
+        "name" => "QC",
+        "code" => "QC",
+        "status" => "active"
+    ]);
+
+    Transaction::create([
+        "name" => "Sale Return",
+        "code" => "SR",
+        "status" => "active"
+    ]);
 });
 
 Route::get("create-accounts", function() {
