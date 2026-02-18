34862d89813263718480e63420b4b1620b10ff14
naveed-ul-llah <naveed@Innovative.net>
2026-02-18 10:36:26 +0500

    fff

--- app/Helpers/paymentCalculationHelpers.php
+++ app/Helpers/paymentCalculationHelpers.php
@@ -452,7 +452,7 @@ function calculatePohaunchDeductions($loadingInfo, $samplingData, $ratePerKg, $t
 
     // Other calculations
      $bagWeightInKgSum = $ratePerKg * ($loadingInfo['bag_weight'] * $loadingInfo['no_of_bags']);
-    $bagWeightInKgSum = 0;
+    // $bagWeightInKgSum = 0;
     // $loadingWeighbridgeSum = $loadingInfo['kanta_charges'] / 2;
     $loadingWeighbridgeSum = 0;
     // $bagsRateSum = $loadingInfo['bag_rate'] * $loadingInfo['no_of_bags'];
