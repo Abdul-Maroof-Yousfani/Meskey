4fa809efa90fa0a04845c0547fbefba887e4c6f3
naveed-ul-llah <naveed@Innovative.net>
2026-04-02 12:35:57 +0500

    Dash Fixes

--- resources/views/management/dashboard/snippets/list_data.blade.php
+++ resources/views/management/dashboard/snippets/list_data.blade.php
@@ -198,6 +198,14 @@
             "paging": false,   // No pagination
             "searching": true,  // Search enabled
                 "ordering": false,
+                   "language": {
+            "emptyTable": "No data found for the selected criteria",
+            "zeroRecords": "No matching records found",
+            "search": "Search:",
+            "info": "Showing _START_ to _END_ of _TOTAL_ entries",
+            "infoEmpty": "Showing 0 to 0 of 0 entries",
+            "infoFiltered": "(filtered from _MAX_ total records)"
+        }
 
         });
     });
