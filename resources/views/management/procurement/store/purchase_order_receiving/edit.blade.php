bffcbff564625ae6c35bffb8657502c0cb7eb604
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-04-13 17:07:00 +0500

    decreased qc's width a bit

--- resources/views/management/procurement/store/purchase_order_receiving/edit.blade.php
+++ resources/views/management/procurement/store/purchase_order_receiving/edit.blade.php
@@ -154,9 +154,9 @@
                 </thead>
                 <tbody id="purchaseRequestBody">
                     @foreach ($purchaseOrderReceiving->purchaseOrderReceivingData ?? [] as $key => $data)
-                             <button id="modalButton{{ $key }}" style="visibility: hidden;" onclick="openModal(this, '{{ route('store.qc.show-create', ['id' => $data->id, 'grn' => optional($purchaseOrderReceiving)->reference_no]) }}', 'Add QC', false, '100%')">&nbsp;</button>
-                      <button id="modalButtonQc{{ $key }}" style="visibility: hidden;" onclick="openModal(this, '{{ route('store.qc.edit', ['id' => $data->id, 'grn' => optional($purchaseOrderReceiving)->reference_no]) }}', 'Edit QC', false, '100%')">&nbsp;</button>
-                      <button id="modalButtonViewQc{{ $key }}" style="visibility: hidden;" onclick="openModal(this, '{{ route('store.qc.view', ['id' => $data->id, 'grn' => optional($purchaseOrderReceiving)->reference_no]) }}', 'View QC', false, '100%')">&nbsp;</button>
+                             <button id="modalButton{{ $key }}" style="visibility: hidden;" onclick="openModal(this, '{{ route('store.qc.show-create', ['id' => $data->id, 'grn' => optional($purchaseOrderReceiving)->reference_no]) }}', 'Add QC', false, '95%')">&nbsp;</button>
+                      <button id="modalButtonQc{{ $key }}" style="visibility: hidden;" onclick="openModal(this, '{{ route('store.qc.edit', ['id' => $data->id, 'grn' => optional($purchaseOrderReceiving)->reference_no]) }}', 'Edit QC', false, '95%')">&nbsp;</button>
+                      <button id="modalButtonViewQc{{ $key }}" style="visibility: hidden;" onclick="openModal(this, '{{ route('store.qc.view', ['id' => $data->id, 'grn' => optional($purchaseOrderReceiving)->reference_no]) }}', 'View QC', false, '95%')">&nbsp;</button>
              
                         <tr id="row_{{ $key }}">
                             <td style="min-width: 300px;">
