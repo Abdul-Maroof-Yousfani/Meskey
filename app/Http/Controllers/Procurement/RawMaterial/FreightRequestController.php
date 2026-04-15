81543ec478e1066a653568cb624d1467749e06fb
naveed-ul-llah <naveed@Innovative.net>
2026-04-15 18:05:32 +0500

    ffff

--- app/Http/Controllers/Procurement/RawMaterial/FreightRequestController.php
+++ app/Http/Controllers/Procurement/RawMaterial/FreightRequestController.php
@@ -137,7 +137,7 @@ class FreightRequestController extends Controller
             $requestStatus = NULL;
 
             $isWithoutContract = false;
-            foreach ($ticket->paymentRequestData->where('module_type', 'freight_payment') as $data) {
+            foreach ($ticket->paymentRequestData->where('request_type', 'freight_payment') as $data) {
                 $totalAmount = $data->total_amount ?? 0;
                 $paidAmount = $data->paid_amount ?? 0;
 
@@ -244,6 +244,7 @@ class FreightRequestController extends Controller
         })
             ->where('status', '!=', 'rejected')
             ->where('module_type', 'freight_payment')
+            ->where('request_type', 'freight_payment')
             ->sum('amount');
 
 
