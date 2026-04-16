59f4ff913530d4f81a076b8d849db65487b5ab51
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-04-16 15:59:19 +0500

    SIT vehicle filters

--- resources/views/management/procurement/raw_material/sit_vehicles/index.blade.php
+++ resources/views/management/procurement/raw_material/sit_vehicles/index.blade.php
@@ -1,103 +1,131 @@
- @extends('management.layouts.master')
- @section('title')
-     Stock in Transit Vehicles
- @endsection
- @section('content')
-     <div class="content-wrapper">
-         <section id="extended">
-             <div class="row w-100 mx-auto">
-                 <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                     <h2 class="page-title">Stock in Transit Vehicles</h2>
-                 </div>
-             </div>
-             <div class="row">
-                 <div class="col-12">
-                     <div class="card">
-                         <div class="card-header">
-                             <form id="filterForm" class="form">
-                                 <div class="row ">
-                                     <div class="col-md-12 my-1">
-                                         <div class="row justify-content-end text-right0">
-                                             <div class="col-md-2">
-                                                 <div class="form-group">
-                                                     <label>Date:</label>
-                                                     <input type="text" name="daterange" class="form-control"
-                                                         value="{{ request('daterange', \Carbon\Carbon::now()->subMonth()->format('m/d/Y') . ' - ' . \Carbon\Carbon::now()->format('m/d/Y')) }}" />
-                                                 </div>
-                                             </div>
-                                             <div class="col-md-2">
-                                                 <div class="form-group">
-                                                     <label>Location:</label>
-                                                     <select name="company_location_id" id="company_location"
-                                                         class="form-control select2">
-                                                         <option value="">Location</option>
-                                                     </select>
-                                                 </div>
-                                             </div>
-                                             <div class="col-md-2">
-                                                 <div class="form-group">
-                                                     <label>Suppliers:</label>
-                                                     <select name="supplier_id" id="supplier_id_f"
-                                                         class="form-control select2">
-                                                         <option value="">Supplier</option>
-                                                     </select>
-                                                 </div>
-                                             </div>
-                                             <div class="col-md-2">
-                                                 <label for="customers" class="form-label">Search</label>
-                                                 <input type="hidden" name="page" value="{{ request('page', 1) }}">
-                                                 <input type="hidden" name="per_page"
-                                                     value="{{ request('per_page', 25) }}">
-                                                 <input type="text" class="form-control" id="search"
-                                                     placeholder="Search here" name="search"
-                                                     value="{{ request('search', '') }}">
-                                             </div>
-                                         </div>
-                                     </div>
-                                 </div>
-                             </form>
-                         </div>
-                         <div class="card-content">
-                             <div class="card-body table-responsive" id="filteredData">
-                                 <table class="table m-0">
-                                     <thead>
-                                         <tr>
-                                             <th class="col-sm-4">Ticket</th>
-                                             <th class="col-sm-1">Location</th>
-                                             <th class="col-sm-2">Created By</th>
-                                             <th class="col-sm-2">Created</th>
-                                             <th class="col-sm-1">Action</th>
-                                         </tr>
-                                     </thead>
-                                 </table>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-             </div>
-         </section>
-     </div>
- @endsection
+@extends('management.layouts.master')
+@section('title')
+    Stock in Transit Vehicles
+@endsection
+@section('content')
+    <div class="content-wrapper">
+        <section id="extended">
+            <div class="row w-100 mx-auto">
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    <h2 class="page-title">Stock in Transit Vehicles</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <form id="filterForm" class="form">
+                                <div class="row mx-0">
+                                    <div class="px-1 text-left" style="width: 12%;">
+                                        <label for="daterange" class="form-label">Date</label>
+                                        <input type="text" name="daterange" id="daterange" class="form-control"
+                                            value="{{ request('daterange', \Carbon\Carbon::now()->subMonth()->format('m/d/Y') . ' - ' . \Carbon\Carbon::now()->format('m/d/Y')) }}" />
+                                    </div>
+                                    <div class="px-1 text-left" style="width: 10%;">
+                                        <label for="company_location" class="form-label">Location</label>
+                                        <select name="company_location_id" id="company_location"
+                                            class="form-control select2">
+                                            <option value="">Location</option>
+                                        </select>
+                                    </div>
+                                    <div class="px-1 text-left" style="width: 10%;">
+                                        <label for="contract_no" class="form-label">Contract No</label>
+                                        <input type="text" name="contract_no" id="contract_no" class="form-control"
+                                            placeholder="Contract" value="{{ request('contract_no', '') }}" />
+                                    </div>
+                                    <div class="px-1 text-left" style="width: 12%;">
+                                        <label for="supplier_id_f" class="form-label">Supplier</label>
+                                        <select name="supplier_id" id="supplier_id_f"
+                                            class="form-control select2">
+                                            <option value="">Supplier</option>
+                                        </select>
+                                    </div>
+                                    <div class="px-1 text-left" style="width: 12%;">
+                                        <label for="broker_id_f" class="form-label">Broker</label>
+                                        <select name="broker_id" id="broker_id_f"
+                                            class="form-control select2">
+                                            <option value="">Broker</option>
+                                        </select>
+                                    </div>
+                                    <div class="px-1 text-left" style="width: 12%;">
+                                        <label for="search" class="form-label">Product</label>
+                                        <input type="text" class="form-control" id="search"
+                                            placeholder="Product" name="search"
+                                            value="{{ request('search', '') }}">
+                                    </div>
+                                    <div class="px-1 text-left" style="width: 12%;">
+                                        <label class="form-label">Vehicle (Truck/Bilty)</label>
+                                        <div class="d-flex">
+                                            <input type="text" name="truck_no" id="truck_no" class="form-control mr-1"
+                                                placeholder="Truck" value="{{ request('truck_no', '') }}" />
+                                            <input type="text" name="bilty_no" id="bilty_no" class="form-control"
+                                                placeholder="Bilty" value="{{ request('bilty_no', '') }}" />
+                                        </div>
+                                    </div>
+                                    <div class="px-1 text-left" style="width: 10%;">
+                                        <label for="status_f" class="form-label">Status</label>
+                                        <select name="status" id="status_f" class="form-control select2">
+                                            <option value="">Status</option>
+                                            <option value="pending">Pending</option>
+                                            <option value="completed">Completed</option>
+                                        </select>
+                                    </div>
+                                    <div class="px-1 text-left" style="width: 10%;">
+                                        <label class="form-label">Actions</label>
+                                        <input type="hidden" name="page" value="{{ request('page', 1) }}">
+                                        <input type="hidden" name="per_page"
+                                            value="{{ request('per_page', 25) }}">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="card-content">
+                            <div class="card-body table-responsive" id="filteredData">
+                                <table class="table m-0">
+                                    <thead>
+                                        <tr>
+                                            <th style="width: 12%;">Date</th>
+                                            <th style="width: 10%;">Location</th>
+                                            <th style="width: 10%;">Contract No</th>
+                                            <th style="width: 12%;">Supplier</th>
+                                            <th style="width: 12%;">Broker</th>
+                                            <th style="width: 12%;">Product</th>
+                                            <th style="width: 12%;">Vehicle No (Truck/Bilty)</th>
+                                            <th style="width: 10%;">Status</th>
+                                            <th style="width: 10%;">Action</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
 
- @section('script')
-     <script>
-         $(document).ready(function() {
-             initializeDynamicDependentSelect2(
-                 '#company_location',
-                 '#supplier_id_f',
-                 'company_locations',
-                 'name',
-                 'id',
-                 'suppliers',
-                 'company_location_ids',
-                 'name',
-                 true,
-                 false,
-                 true,
-                 true,
-             );
+@section('script')
+    <script>
+        $(document).ready(function() {
+            initializeDynamicDependentSelect2(
+                '#company_location',
+                '#supplier_id_f',
+                'company_locations',
+                'name',
+                'id',
+                'suppliers',
+                'company_location_ids',
+                'name',
+                true,
+                false,
+                true,
+                true,
+            );
+            
+            initializeDynamicSelect2('#broker_id_f', 'brokers', 'name', 'id', true, false, true, true);
 
-             filterationCommon(`{{ route('raw-material.get.sit-vehicle') }}`)
-         });
-     </script>
- @endsection
+            filterationCommon(`{{ route('raw-material.get.sit-vehicle') }}`)
+        });
+    </script>
+@endsection
