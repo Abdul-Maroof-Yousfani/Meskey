be98636ef6d40c316c05f841b4e690d13501da37
naveed-ul-llah <naveed@Innovative.net>
2026-04-17 14:43:21 +0500

    ffff

--- resources/views/management/procurement/raw_material/purchase_order/getList.blade.php
+++ resources/views/management/procurement/raw_material/purchase_order/getList.blade.php
@@ -114,14 +114,18 @@
                 {{ $row->createdByUser->name ?? 'N/A' }}
             </td>
             <td>
+                @canAccess('procurement-raw-purchase-order-edit')
                 <a onclick="openModal(this,'{{ route($row->purchase_type == 'gate_buying' ? 'raw-material.gate-buying.edit' : 'raw-material.purchase-order.edit', $row->id) }}','{{ $row->purchase_type == 'gate_buying' ? 'Edit Gate Buying' : 'Edit Purchase Order' }}')"
                     class="info p-1 text-center mr-2 position-relative">
                     <i class="ft-edit font-medium-3"></i>
                 </a>
+                @endcanAccess
+                @canAccess('procurement-raw-purchase-order-delete')
                 <a onclick="deletemodal('{{ route('raw-material.purchase-order.destroy', $row->id) }}', '{{ route('raw-material.get.purchase-order') }}')"
                     class="danger p-1 text-center mr-2 position-relative">
                     <i class="ft-x font-medium-3"></i>
                 </a>
+                @endcanAccess
             </td>
         </tr>
     @endforeach
