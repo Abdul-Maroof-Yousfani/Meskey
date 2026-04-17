727e37e7778359b1726365f285dd61d7901daeb1
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-04-17 15:35:10 +0500

    test

--- app/Http/Controllers/Procurement/RawMaterial/PurchaseOrderController.php
+++ app/Http/Controllers/Procurement/RawMaterial/PurchaseOrderController.php
@@ -351,7 +351,7 @@ class PurchaseOrderController extends Controller
                 'min_bags' => $data['min_bags'] ?? null,
                 'max_bags' => $data['max_bags'] ?? null,
                 'contract_status' => $data['contract_status'] ?? null,
-                'status' => $data['contract_status'] == 'close-contract-due-to-market-down' || $data['contract_status'] == 'close-with-market-rate-penalty' ? 'cancelled' : 'draft',
+                'status' => $data['contract_status'] == 'close-contract-due-to-market-down' || $data['contract_status'] == 'close-with-market-rate-penalty' ? 'cancelled' : 'completed',
                 'remarks' => $data['remarks'] ?? null,
             ];
 
