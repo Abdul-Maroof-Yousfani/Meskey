b9f9ff90541970e4af84c251dd95a9b1c6c2066c
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-04-24 17:42:56 +0500

    test

--- app/Http/Controllers/Procurement/RawMaterial/TicketContractController.php
+++ app/Http/Controllers/Procurement/RawMaterial/TicketContractController.php
@@ -589,7 +589,7 @@ class TicketContractController extends Controller
             'rejectedHalfArrivalTickets',
             'stockInTransitTickets'
         ])
-            ->where('am_approval_status', 'approved')
+            ->where('status', 'draft')
             ->where('supplier_id', $arrivalTicket->accounts_of_id)
             ->where('company_location_id', $arrivalTicket->location_id)
             ->where('product_id', $arrivalTicket->qc_product);
