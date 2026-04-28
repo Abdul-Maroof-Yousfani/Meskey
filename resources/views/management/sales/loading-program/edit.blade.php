153430c1dc604fd13e3546cab3ff06c1db46badb
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-04-28 11:52:25 +0500

    test

--- resources/views/management/sales/loading-program/edit.blade.php
+++ resources/views/management/sales/loading-program/edit.blade.php
@@ -1243,4 +1243,11 @@
         $doSelect.removeAttr('required');
         $mark.hide();
     }
+
+    $('.select2').on('select2:open', function (e) {
+        // Remove all Select2 scroll blockers from window & parents
+        $(document).off('scroll.select2');
+        $(window).off('scroll.select2');
+        $('*').off('scroll.select2');           // aggressive but often works
+    });
 </script>
