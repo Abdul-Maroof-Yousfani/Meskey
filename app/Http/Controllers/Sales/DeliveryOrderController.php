5279de8da4e6b02c038123e46dabf68bf5431424
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-04-28 13:07:14 +0500

    test

--- app/Http/Controllers/Sales/DeliveryOrderController.php
+++ app/Http/Controllers/Sales/DeliveryOrderController.php
@@ -208,9 +208,7 @@ class DeliveryOrderController extends Controller
                 //     return response()->json("Total balance is $balance. you can not exceed this balance", 422);
                 // }
 
-                dd($remaining_qty);
-
-                if($remaining_qty <= $request->qty[$key]) {
+                if($remaining_qty < $request->qty[$key]) {
                     return response()->json("Total remaining qty(kg): $remaining_qty. you can not exceed this balance", 422);
                 }
 
