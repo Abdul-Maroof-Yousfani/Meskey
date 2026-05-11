633200c575562941d9f2e78679e8c4f36379f570
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-05-11 17:13:11 +0500

    approval issue in purchase quotation

--- app/Models/Procurement/Store/PurchaseQuotationData.php
+++ app/Models/Procurement/Store/PurchaseQuotationData.php
@@ -44,6 +44,12 @@ class PurchaseQuotationData extends Model
     {
         static::updating(
             function ($model) {
+                if ($model->getOriginal('am_approval_status') === 'approved') {
+                    throw \Illuminate\Validation\ValidationException::withMessages([
+                        'am_approval_status' => ['This item has already been approved and cannot be modified.'],
+                    ]);
+                }
+
                 $changes = $model->getDirty();
                 $changedColumns = [];
 
