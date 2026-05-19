366d65f5f6e6b7d5e8a78494d961d3ae16da4988
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-05-19 16:54:58 +0500

    removed redundant button

--- resources/views/management/finance/receipt_voucher/create.blade.php
+++ resources/views/management/finance/receipt_voucher/create.blade.php
@@ -121,9 +121,9 @@
                                     <div class="form-group">
                                         <div class="d-flex justify-content-between align-items-center mb-2">
                                             <label class="mb-0">Selected References</label>
-                                            <button type="button" class="btn btn-sm btn-info" id="add-advance-btn" onclick="addAdvanceRow()">
+                                            <!-- <button type="button" class="btn btn-sm btn-info" id="add-advance-btn" onclick="addAdvanceRow()">
                                                 <i class="fa fa-plus"></i> Add Advance
-                                            </button>
+                                            </button> -->
                                         </div>
                                         <div style="overflow: visible;">
                                             <table class="table table-bordered" id="referencesTable" style="overflow: visible;">
