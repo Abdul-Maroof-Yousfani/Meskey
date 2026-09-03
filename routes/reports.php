1ffe9de53f14adbe94a950b3811aa72eadffc580
naveed-ul-llah <naveed@Innovative.net>
2026-09-03 17:37:00 +0500

    Fxies

--- routes/reports.php
+++ routes/reports.php
@@ -1,12 +1,14 @@
 <?php
 
-use App\Http\Controllers\Reports\ArrivalReportController;
+use App\Http\Controllers\Reports\Arrival\{ArrivalReportController, TruckDetailReportController};
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('arrival')->group(function () {
   Route::resource('arrival-history', ArrivalReportController::class);
   Route::post('/get-arrival-history', [ArrivalReportController::class, 'getArrivalReport'])->name('reports.arrival.get.arrival-history');
+  Route::resource('truck-detail', TruckDetailReportController::class);
+  Route::post('/get-truck-detail', [TruckDetailReportController::class, 'getList'])->name('reports.arrival.get.truck-detail');
 });
 
 
