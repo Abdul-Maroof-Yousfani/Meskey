986abbe30932ba0e1af555de427fd603397094c3
naveed-ul-llah <naveed@Innovative.net>
2026-09-03 11:25:45 +0500

    Fxies

--- resources/views/management/procurement/raw_material/purchase_order/create.blade.php
+++ resources/views/management/procurement/raw_material/purchase_order/create.blade.php
@@ -245,14 +245,14 @@
         <div class="col-xs-4 col-sm-4 col-md-4">
             <div class="form-group ">
                 <label>Rate Per KG:</label>
-                <input type="number" step="0.01" name="rate_per_kg" placeholder="Rate Per KG" class="form-control" />
+                <input type="number" step="0.0001" name="rate_per_kg" placeholder="Rate Per KG" class="form-control" />
 
             </div>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4">
             <div class="form-group ">
                 <label>Rate Per Mound:</label>
-                <input type="number" step="0.01" name="rate_per_mound" placeholder="Rate Per Mound"
+                <input type="number" step="0.0001" name="rate_per_mound" placeholder="Rate Per Mound"
                     class="form-control" />
 
             </div>
@@ -260,7 +260,7 @@
         <div class="col-xs-4 col-sm-4 col-md-4">
             <div class="form-group ">
                 <label>Rate Per 100KG:</label>
-                <input type="number" step="0.01" name="rate_per_100kg" placeholder="Rate Per 100KG"
+                <input type="number" step="0.0001" name="rate_per_100kg" placeholder="Rate Per 100KG"
                     class="form-control" />
             </div>
         </div>
