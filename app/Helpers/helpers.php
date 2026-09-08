eaf67f6cb19e03dd44334aa56325f5c6ed36b36e
Syed Ali <infosyedalimoiz99@gmail.com>
2026-09-08 15:23:28 +0500

    Update helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -2403,7 +2403,7 @@ if (!function_exists('formatDate')) {
      * @param string $default
      * @return string
      */
-    function formatDate($date, string $format = 'd M Y', string $default = '')
+    function formatDate($date, string $format = 'd M Y', string $default = 'N/A')
     {
         return formatDateTime($date, $format, $default);
     }
@@ -2418,7 +2418,7 @@ if (!function_exists('formatTime')) {
      * @param string $default
      * @return string
      */
-    function formatTime($date, string $format = 'h:i:s A', string $default = '')
+    function formatTime($date, string $format = 'h:i:s A', string $default = 'N/A')
     {
         return formatDateTime($date, $format, $default);
     }
