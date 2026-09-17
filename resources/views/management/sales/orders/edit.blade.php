8d650f0341315d8b40f0d4cef786c338a0cbc156
Arsal-INPL <arsal.farooq@innovative-net.com>
2026-09-17 12:16:33 +0500

    changes

--- resources/views/management/sales/orders/edit.blade.php
+++ resources/views/management/sales/orders/edit.blade.php
@@ -76,7 +76,7 @@
                     <div class="form-group">
                         <label class="form-label">Delivery Date:</label>
                         <input type="date" name="delivery_date" value="{{ $sale_order->delivery_date ? \Carbon\Carbon::parse($sale_order->delivery_date)->format('Y-m-d') : '' }}" 
-                            id="delivery_date" class="form-control">
+                            id="delivery_date" class="form-control" readonly>
                     </div>
                 </div>
                 <div class="col-md-6">
